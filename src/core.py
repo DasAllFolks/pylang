c76a56df0fd1e4c3ee812fc00b85d7651aa84621
Steven Das <steven@zirtual.com>
2015-02-27 21:58:57 -0600

    Stub of bag of words method.

---
+++ src/core.py
@@ -0,0 +1,11 @@
+def bag_of_words(text):
+  """Returns bag-of-words representation of the input text.
+
+  Args:
+    text: A string containing the text.
+
+  Returns:
+    A dictionary of strings to integers.
+  """
+  # XXXX! Write me!
+  pass
